0e89e92fe154168125cf603366d304213c272112
Matilda Rutherford <rutherford.matilda@gmail.com>
2018-05-15 10:54:24 +1000

    heroku begin

--- main.rb
+++ main.rb
@@ -2,11 +2,8 @@ require 'sinatra'
 
 require 'pry'
 require_relative 'models/carrierwave-config'
-# require 'active_record'
+
 require 'carrierwave'
-# require 'carrierwave/orm/activerecord'
-# require 'cloudinary'
-# require_relative 'models/asset'
 
 
 
