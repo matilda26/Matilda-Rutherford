9ab1a62ca9bf361fd9754fe3c3a8952b718c94f6
Matilda Rutherford <rutherford.matilda@gmail.com>
2018-05-16 14:18:19 +1000

    update

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,29 +1,27 @@
-// var infoBtn = document.querySelector(".project-info-btn");
-// var infoDiv = document.querySelector(".overlay");
-//
-//
-// infoBtn.addEventListener('click', function() {
-//   infoDiv.style.width = "50%";
-// })
-// document.querySelector(".exit").addEventListener('click', function() {
-//   infoDiv.style.width="0%";
-// })
-//
-//
-// var navBtn = document.querySelector(".nav-btn");
-// var navDiv = document.querySelector(".nav-overlay");
-//
-// navBtn.addEventListener('click', function() {
-//   navDiv.style.width = "50%";
-// })
-// document.querySelector(".nav-exit").addEventListener('click', function() {
-//   navDiv.style.width="0%";
-// })
+
 var degrees = 45;
 var infoBtns = document.querySelectorAll(".project-info-btn");
 infoBtns.forEach(function(button) {
   button.addEventListener('click', function(e) {
     e.target.parentNode.children[0].classList.toggle("project-vis");
+    e.target.classList.toggle("btn-rotate");
+  })
+})
+
+var minBtns = document.querySelectorAll(".project-min-btn");
+minBtns.forEach(function(button) {
+  button.addEventListener('click', function(e) {
+    if (e.target.parentElement.children[0].style.display === "none") {
+      e.target.parentElement.children[0].style.display = "block";
+      e.target.parentElement.children[1].style.display = "block";
+      e.target.parentElement.children[2].style.display = "block";
+    } else {
+      e.target.parentElement.children[2].style.display = "none";
+      e.target.parentElement.children[0].style.display = "none";
+      e.target.parentElement.children[1].style.display = "none";
+
+    }
+    e.target.parentElement.classList.toggle("project-min");
     e.target.classList.toggle("btn-rotate");
   })
 })
