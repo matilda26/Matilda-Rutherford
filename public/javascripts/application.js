fc0c140b1912a9e36058422fd32c3e24e25a772a
Matilda Rutherford <rutherford.matilda@gmail.com>
2018-05-16 12:50:36 +1000

    change to projects view

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,21 +1,32 @@
-var infoBtn = document.querySelector(".project-info-btn");
-var infoDiv = document.querySelector(".overlay");
-
-
-infoBtn.addEventListener('click', function() {
-  infoDiv.style.width = "50%";
-})
-document.querySelector(".exit").addEventListener('click', function() {
-  infoDiv.style.width="0%";
-})
-
-
-var navBtn = document.querySelector(".nav-btn");
-var navDiv = document.querySelector(".nav-overlay");
-
-navBtn.addEventListener('click', function() {
-  navDiv.style.width = "50%";
-})
-document.querySelector(".nav-exit").addEventListener('click', function() {
-  navDiv.style.width="0%";
+// var infoBtn = document.querySelector(".project-info-btn");
+// var infoDiv = document.querySelector(".overlay");
+//
+//
+// infoBtn.addEventListener('click', function() {
+//   infoDiv.style.width = "50%";
+// })
+// document.querySelector(".exit").addEventListener('click', function() {
+//   infoDiv.style.width="0%";
+// })
+//
+//
+// var navBtn = document.querySelector(".nav-btn");
+// var navDiv = document.querySelector(".nav-overlay");
+//
+// navBtn.addEventListener('click', function() {
+//   navDiv.style.width = "50%";
+// })
+// document.querySelector(".nav-exit").addEventListener('click', function() {
+//   navDiv.style.width="0%";
+// })
+var degrees = 45;
+var infoBtns = document.querySelectorAll(".project-info-btn");
+infoBtns.forEach(function(button) {
+  button.addEventListener('click', function(e) {
+    e.target.parentNode.children[0].classList.toggle("project-vis");
+    e.target.classList.toggle("btn-rotate");
+    // button.style.transition = "0.2s";
+    // button.style.transform = "rotate("+ degrees +"deg)";
+    // degrees = degrees + 45;
+  })
 })
