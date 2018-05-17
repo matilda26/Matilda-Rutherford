9234498043cc66d48b6068cedff67fa6eddba936
Matilda Rutherford <rutherford.matilda@gmail.com>
2018-05-17 14:49:19 +1000

    update to initial screen

--- application.js
+++ application.js
@@ -23,17 +23,16 @@ infoBtns.forEach(function(button) {
 var minBtns = document.querySelectorAll(".project-min-btn");
 minBtns.forEach(function(button) {
   button.addEventListener('click', function(e) {
-    if (e.target.parentElement.children[0].style.display === "none") {
-      e.target.parentElement.children[0].style.display = "block";
-      e.target.parentElement.children[1].style.display = "block";
-      e.target.parentElement.children[2].style.display = "block";
-    } else {
-      e.target.parentElement.children[2].style.display = "none";
+    if (e.target.parentElement.children[0].style.display === "block") {
       e.target.parentElement.children[0].style.display = "none";
       e.target.parentElement.children[1].style.display = "none";
-
+      e.target.parentElement.children[2].style.display = "none";
+    } else {
+      e.target.parentElement.children[2].style.display = "block";
+      e.target.parentElement.children[0].style.display = "block";
+      e.target.parentElement.children[1].style.display = "block";
     }
-    e.target.parentElement.classList.toggle("project-min");
+    // e.target.parentElement.classList.toggle("project-min");
     e.target.classList.toggle("btn-rotate");
   })
 })
